75dd2d1cf3d3181d734461e94e6f7d2f4231410e
phamquy2003 <anhquyk797@gmail.com>
2024-05-20 20:23:12 +0700

    abcccc

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BTL_NguyenDangPhuong_2051063453")]
+[assembly: AssemblyTitle("BTL_PhamVanQuy_2151163718")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BTL_NguyenDangPhuong_2051063453")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("BTL_PhamVanQuy_2151163718")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
